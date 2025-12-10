40db718f1fce1c712b33de0280fce5e1ebb05b72
ripplenet29130 <cev29130@gmail.com>
2025-12-10 11:18:15 +0900

    Update Tooltip.tsx

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 
 interface TooltipProps {
-  label: string;        // 表示するキーワード
-  description: string;  // ホバーした時に表示する説明
+  label: string;
+  description: string;
 }
 
 const Tooltip: React.FC<TooltipProps> = ({ label, description }) => {
   return (
     <span className="relative group cursor-help font-semibold text-gray-700">
       {label}
+
+      {/* ツールチップ本体 */}
       <div
-        className="absolute left-1/2 -translate-x-1/2 mt-2 hidden group-hover:block
-                   bg-black text-white text-xs p-2 rounded shadow-lg w-56 z-50"
+        className="
+          absolute left-1/2 -translate-x-1/2 mt-2
+          opacity-0 pointer-events-none
+          group-hover:opacity-100 group-hover:pointer-events-auto
+          transition-opacity duration-150
+          bg-black text-white text-xs p-2 rounded shadow-lg w-56 z-50
+        "
       >
         {description}
       </div>
